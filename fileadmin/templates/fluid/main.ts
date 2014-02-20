d3bedd7dd970ea38f6a869780917c247c25d6138
mattanja <mattanja.kern@gmail.com>
2014-02-20 22:25:57 +0100

    fix jquery inclusion

--- fileadmin/templates/fluid/main.ts
+++ fileadmin/templates/fluid/main.ts
@@ -7,50 +7,59 @@
 #
 
 page = PAGE
+page {
 
-# <!DOCTYPE html>
-# <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
-# <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
-# <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
-# <!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
+	# <!DOCTYPE html>
+	# <!--[if lt IE 7]>      <html class="no-js lt-ie9 lt-ie8 lt-ie7"> <![endif]-->
+	# <!--[if IE 7]>         <html class="no-js lt-ie9 lt-ie8"> <![endif]-->
+	# <!--[if IE 8]>         <html class="no-js lt-ie9"> <![endif]-->
+	# <!--[if gt IE 8]><!--> <html class="no-js"> <!--<![endif]-->
 
-page.includeCSS {
-	file1 = fileadmin/templates/web/css/bootstrap.min.css
-	file11 = fileadmin/templates/web/css/bootstrap-theme.min.css
-	file12 = fileadmin/templates/web/css/main.css
-}
+	includeCSS {
+		file1 = fileadmin/templates/web/css/bootstrap.min.css
+		file11 = fileadmin/templates/web/css/bootstrap-theme.min.css
+		file12 = fileadmin/templates/web/css/main.css
+	}
 
-# <script>window.jQuery || document.write('<script src="{f:uri.resource(path:'../web/js/vendor/jquery-1.10.1.min.js')}"><\/script>')</script>
-# <script src="js/vendor/bootstrap.min.js"></script>
-# <script src="js/main.js"></script>
-# Check out: https://github.com/FluidTYPO3/vhs
-page.includeJS {
-	file1 = fileadmin/templates/web/js/vendor/modernizr-2.6.2-respond-1.1.0.min.js
-	#CDN file10 = fileadmin/templates/web/js/vendor/jquery-1.10.1.min.js
-	file11 = fileadmin/templates/web/js/vendor/bootstrap.min.js
-	file12 = fileadmin/templates/web/js/main.js
-}
+	includeJSlibs {
+		jquery = //ajax.googleapis.com/ajax/libs/jquery/1.11.0/jquery.min.js
+		jqery {
+			external = 1
+			forceOnTop = 1
+		}
+	}
 
-page.10 = FLUIDTEMPLATE
-page.10 {
-	template = FILE
-	template.file = fileadmin/templates/fluid/main.htm
-	partialRootPath = fileadmin/templates/fluid/partials/
-	layoutRootPath = fileadmin/templates/fluid/layouts/
-	variables {
-		#headerLogo < temp.headerLogo
-		#headerNavi < temp.headerNavi
-		#printHeader < temp.printHeader
-		#leftCol < styles.content.getLeft
-		#mainNavi < temp.mainNavi
-		#subNavi < temp.subNavi
-		#rootlineNavi < temp.rootlineNavi
-		contentGet < styles.content.get
-		#footerText < temp.footerText
-		#footerNavi < temp.footerNavi
+	# <script>window.jQuery || document.write('<script src="{f:uri.resource(path:'../web/js/vendor/jquery-1.10.1.min.js')}"><\/script>')</script>
+	# <script src="js/vendor/bootstrap.min.js"></script>
+	# <script src="js/main.js"></script>
+	# Check out: https://github.com/FluidTYPO3/vhs
+	includeJS {
+		file1 = fileadmin/templates/web/js/vendor/modernizr-2.6.2-respond-1.1.0.min.js
+		#CDN file10 = fileadmin/templates/web/js/vendor/jquery-1.10.1.min.js
+		file11 = fileadmin/templates/web/js/vendor/bootstrap.min.js
+		file12 = fileadmin/templates/web/js/main.js
+	}
+
+	10 = FLUIDTEMPLATE
+	10 {
+		template = FILE
+		template.file = fileadmin/templates/fluid/main.htm
+		partialRootPath = fileadmin/templates/fluid/partials/
+		layoutRootPath = fileadmin/templates/fluid/layouts/
+		variables {
+			#headerLogo < temp.headerLogo
+			#headerNavi < temp.headerNavi
+			#printHeader < temp.printHeader
+			#leftCol < styles.content.getLeft
+			#mainNavi < temp.mainNavi
+			#subNavi < temp.subNavi
+			#rootlineNavi < temp.rootlineNavi
+			contentGet < styles.content.get
+			#footerText < temp.footerText
+			#footerNavi < temp.footerNavi
+		}
 	}
 }
-
 
 
 json = PAGE
